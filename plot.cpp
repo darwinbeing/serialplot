56d2be1c0ef771a95e7bea556e53f32b588ccead
Hasan Yavuz ?ZDERYA <hy@ozderya.net>
2015-09-19 22:24:02 +0300

    take snapshot button creates a new snapshot and opens its window

---
+++ plot.cpp
@@ -0,0 +1,112 @@
+/*
+  Copyright © 2015 Hasan Yavuz Özderya
+
+  This file is part of serialplot.
+
+  serialplot is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  serialplot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with serialplot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QRectF>
+#include "plot.h"
+Plot::Plot(QWidget* parent) :
+    QwtPlot(parent),
+    zoomer(this->canvas(), false),
+    sZoomer(this, &zoomer)
+{
+    isAutoScaled = false;
+
+    QObject::connect(&zoomer, &Zoomer::unzoomed, this, &Plot::unzoomed);
+
+    zoomer.setZoomBase();
+    grid.attach(this);
+
+    rectItem.setRect(QRectF(0,0,100,1));
+    // rectItem.attach(this);
+
+    darkBackground(false);
+}
+
+void Plot::setAxis(bool autoScaled, double yAxisMin, double yAxisMax)
+{
+    this->isAutoScaled = autoScaled;
+
+    if (!autoScaled)
+    {
+        yMin = yAxisMin;
+        yMax = yAxisMax;
+    }
+
+    zoomer.zoom(0);
+    resetAxes();
+}
+
+void Plot::resetAxes()
+{
+    if (isAutoScaled)
+    {
+        setAxisAutoScale(QwtPlot::yLeft);
+    }
+    else
+    {
+        setAxisScale(QwtPlot::yLeft, yMin, yMax);
+    }
+
+    replot();
+}
+
+void Plot::unzoomed()
+{
+    setAxisAutoScale(QwtPlot::xBottom);
+    resetAxes();
+}
+
+void Plot::showGrid(bool show)
+{
+    grid.enableX(show);
+    grid.enableY(show);
+    replot();
+}
+
+void Plot::showMinorGrid(bool show)
+{
+    grid.enableXMin(show);
+    grid.enableYMin(show);
+    replot();
+}
+
+void Plot::unzoom()
+{
+    zoomer.zoom(0);
+}
+
+void Plot::darkBackground(bool enabled)
+{
+    if (enabled)
+    {
+        setCanvasBackground(QBrush(Qt::black));
+        grid.setPen(Qt::darkGray);
+        zoomer.setRubberBandPen(QPen(Qt::white));
+        zoomer.setTrackerPen(QPen(Qt::white));
+        sZoomer.setPickerPen(QPen(Qt::white));
+    }
+    else
+    {
+        setCanvasBackground(QBrush(Qt::white));
+        grid.setPen(Qt::lightGray);
+        zoomer.setRubberBandPen(QPen(Qt::black));
+        zoomer.setTrackerPen(QPen(Qt::black));
+        sZoomer.setPickerPen(QPen(Qt::black));
+    }
+    replot();
+}
---
+++ serialplot.pro
@@ -0,0 +1,71 @@
+#
+# Copyright © 2015 Hasan Yavuz Özderya
+#
+# This file is part of serialplot.
+#
+# serialplot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# serialplot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with serialplot.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-04T08:20:06
+#
+#-------------------------------------------------
+
+QT       += core gui serialport
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = serialplot
+TEMPLATE = app
+
+CONFIG += qwt
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    portcontrol.cpp \
+    plot.cpp \
+    zoomer.cpp \
+    hidabletabwidget.cpp \
+    framebuffer.cpp \
+    scalepicker.cpp \
+    scalezoomer.cpp \
+    portlist.cpp \
+    snapshotview.cpp
+
+HEADERS  += mainwindow.h \
+    utils.h \
+    portcontrol.h \
+    floatswap.h \
+    plot.h \
+    zoomer.h \
+    hidabletabwidget.h \
+    framebuffer.h \
+    scalepicker.h \
+    scalezoomer.h \
+    portlist.h \
+    snapshotview.h
+
+FORMS    += mainwindow.ui \
+    about_dialog.ui \
+    portcontrol.ui \
+    snapshotview.ui
+
+INCLUDEPATH += qmake/
+
+CONFIG += c++11
+
+RESOURCES += \
+    misc/icons.qrc
